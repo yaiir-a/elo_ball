7e90ce91c2a7bf95a9e01f6615ecf5f31d8cfcaa
yaya <yka@gmail.com>
2018-12-30 12:10:44 +0000

    connected to test db

--- elo_ball_app.py
+++ elo_ball_app.py
@@ -16,7 +16,14 @@ prod_db_creds = {'host':'yaiir.mysql.pythonanywhere-services.com',
                 'passwd':MYSQL_PASSWORD,
                 'database':"yaiir$gamerecords"}
 
-db = MySQLDatabase(**prod_db_creds)
+test_db_creds = {'host':'yaiir.mysql.pythonanywhere-services.com',
+                'user':"yaiir",
+                'passwd':MYSQL_PASSWORD,
+                'database':"yaiir$test-gamerecords"}
+
+
+
+db = MySQLDatabase(**test_db_creds)
 
 @app.before_request
 def _db_connect():
